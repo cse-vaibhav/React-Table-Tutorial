07ccf878736b9e1ed222292c591eaf0a3b67bb6a
Vaibhav Jain <jainvaibhav671@gmail.com>
2023-07-24 10:24:15 +0530

    Added Table with Column Groups

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BasicTable } from "./components/Table/Basic"
+import { ColumnGroupsTable } from "./components/Table/ColumnGroups"
 
 export default function App() {
   return <>
     <h1>Basic Table</h1>
     <BasicTable />
+
+    <h1>Column Groups Table</h1>
+    <ColumnGroupsTable />
   </>
 }
 
---
+++ src/components/Table/ColumnGroups.tsx
@@ -0,0 +1,154 @@
+import { useReducer, useState } from 'react'
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
+
+//{{{ Dummy Data
+type Person = {
+  firstName: string,
+  lastName: string,
+  age: number,
+  visits: number,
+  status: string,
+  progress: number
+}
+
+const defaultData: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'Vaibhav',
+    lastName: 'Jain',
+    age: 20,
+    visits: 200,
+    status: 'In Relationship',
+    progress: 80,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+]//}}}
+
+//{{{ Columns
+const columnHelper = createColumnHelper<Person>()
+const columns = [
+  //{{{ Name
+  columnHelper.group({
+    id: "Hello",
+    header: () => <span>Name</span>,
+    columns: [
+      columnHelper.accessor("firstName", {
+        header: () => <span>First Name</span>,
+        cell: info => info.getValue(),
+        footer: info => info.column.id
+      }),
+      columnHelper.accessor("lastName", {
+        header: () => <span>Last Name</span>,
+        cell: info => info.getValue(),
+        footer: info => info.column.id
+      }),    
+    ]
+  }),//}}}
+  
+  //{{{ Info
+  columnHelper.group({
+    header: "Info",
+    footer: props => props.column.id,
+    columns: [
+      columnHelper.accessor("age", {
+        header: () => <span>Age</span>,
+        cell: info => info.getValue(),
+        footer: info => info.column.id
+      }),
+      columnHelper.group({
+        header: "More Info",
+        footer: props => props.column.id,
+        columns: [
+          columnHelper.accessor('visits', {
+            header: () => <span>Visits</span>,
+            footer: info => info.column.id,
+          }),
+          columnHelper.accessor('status', {
+            header: 'Status',
+            footer: info => info.column.id,
+          }),
+          columnHelper.accessor('progress', {
+            header: 'Profile Progress',
+            footer: info => info.column.id,
+          }),
+        ]
+      })
+      
+    ]
+  })//}}}
+
+]//}}}
+
+export function ColumnGroupsTable() {
+
+  const [data, setData] = useState(() => [...defaultData])
+  const rerender = useReducer(() => ({}), {})[1];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
+
+  return (
+    <>
+      <div className='p-4'>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className='h-4' />
+      <button onClick={() => rerender()} className="border p-2">
+        Rerender
+      </button>
+    </>
+  )
+}
